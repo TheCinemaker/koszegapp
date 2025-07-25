9066386d2b622bbb1f481b2c1e370aa6ffcc6aba
TheCinemaker <31615891+TheCinemaker@users.noreply.github.com>
2025-07-25 08:50:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { DarkModeProvider } from './contexts/DarkModeContext';
 import App from './App';
+
 import './index.css';
 import './i18n';
+import 'leaflet/dist/leaflet.css'; // <-- IDE SZÚRD BE, EZ LESZ AZ EGYETLEN HELY!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
