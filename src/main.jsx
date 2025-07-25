9f189b7731b4555285257c7333e6b8b0aee23c89
TheCinemaker <31615891+TheCinemaker@users.noreply.github.com>
2025-07-25 09:27:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,19 @@ import App from './App';
 
 import './index.css';
 import './i18n';
-import 'leaflet/dist/leaflet.css'; // <-- IDE SZÚRD BE, EZ LESZ AZ EGYETLEN HELY!
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
