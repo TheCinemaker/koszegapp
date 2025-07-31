cd362e5235faab0ac4ac51853f559a699c179a3a
TheCinemaker <31615891+TheCinemaker@users.noreply.github.com>
2025-07-31 17:10:10 +0200

    Update icons.js

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -2,7 +2,7 @@ import L from 'leaflet';
 
 export const machineIcon = new L.Icon({
   iconUrl: '/images/parking_meter.png', /
-  iconRetinaUrl: '/images/parking_meter_icon.png',
+  iconRetinaUrl: '/images/parking_meter.png',
   iconAnchor: [15, 30],   // Az ikon "hegye"
   popupAnchor: [0, -30], // A popup pozíciója az ikonhoz képest
   shadowUrl: null,      
