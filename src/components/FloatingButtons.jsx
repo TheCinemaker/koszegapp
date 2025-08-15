b50925429d270cb571f543c6a335a3dc024eb90b
TheCinemaker <31615891+TheCinemaker@users.noreply.github.com>
2025-08-15 14:25:24 +0200

    Update FloatingButtons.jsx

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -1,13 +1,13 @@
 import ScrollToTop from './ScrollToTop';
 import BackToHome from './BackToHome';
-import ChatButton from './ChatButton';
+{/* import ChatButton from './ChatButton'; /> */}
 
 export default function FloatingButtons() {
   return (
     <div className="fixed bottom-[72px] right-4 z-[60] flex flex-col items-end gap-1.5">
       <ScrollToTop />
       <BackToHome />
-      <ChatButton />
+     {/* <ChatButton /> */}
     </div>
   );
 }
